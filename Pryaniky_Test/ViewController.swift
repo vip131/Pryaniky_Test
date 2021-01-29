716684a62825b902deedfd4c9f172c17a504808b
Dmitry Chaschin <vip131@list.ru>
2021-01-29 17:03:34 +0300

    Initial Commit

---
+++ Pryaniky_Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pryaniky_Test
+//
+//  Created by Admin on 29.01.2021.
+//  Copyright © 2021 DmitryChaschin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
